c07e823d64d6d8a1931ae880f04f21c46f8b5e6c
alnonymus <yougottamove9@gmail.com>
2024-08-16 12:26:36 +0200

    alnonymus.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alnonymus.py
@@ -0,0 +1,25 @@
+
+"""
+https://www.python.org/
+
+"""
+
+# https://www.python.org/
+
+
+to_visit = "python website"
+
+LAN_NAME = print("Hello, Python!")
+
+
+al = 46
+
+a_l = 46.0
+
+al_boo_t = True
+
+al_boo_f = False
+
+al_str = "46"
+
+LAN_NAME
